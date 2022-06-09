a8ae26dbe1d7ebf0cbcbe7eeb1211560796960bc
qwqw0414 <xzdi0414@gmail.com>
2022-06-09 14:05:07 +0900

    Logging 관련 aop, filter 등록, 각종 유틸 등록

---
+++ src/main/java/com/joje/dbee/common/filter/LoggerFilter.java
@@ -0,0 +1,62 @@
+package com.joje.dbee.common.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+import com.google.gson.Gson;
+import com.joje.dbee.common.utils.HttpUtil;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@WebFilter(urlPatterns = "/*")
+public class LoggerFilter implements Filter{
+
+	private static final String[] EXCLUDE_PATHS = { "assets" };
+	private static Gson gson = new Gson();
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		String url = httpRequest.getRequestURI();
+		String method = httpRequest.getMethod();
+		String contentType = request.getContentType();
+
+		if (isInclude(url)) {
+			// Request Logging
+			log.info("=================== >> {} START {} >> ===================", method, url);
+			log.debug("ContentType : {}", contentType);
+			if (contentType != null && contentType.startsWith("application/json")) {
+				ReadableRequestWrapper requestWrapper = new ReadableRequestWrapper((HttpServletRequest) request);
+				log.debug("RequestBody : {}", HttpUtil.getBody(requestWrapper));
+			} else {
+				log.debug("ParameterMap : {}", gson.toJson(request.getParameterMap()));
+			}
+
+			chain.doFilter(request, response);
+
+			log.debug("=================== << {} END {} << ===================\n", method, url);
+		} else {
+			chain.doFilter(request, response);
+		}
+	}
+
+//	Exclude URL 유효성 검사
+	private boolean isInclude(String url) {
+		for (String i : EXCLUDE_PATHS) {
+			if (url.contains(i))
+				return false;
+		}
+		return true;
+	}
+
+}
---
+++ src/main/java/com/joje/dbee/common/utils/StringUtil.java
@@ -0,0 +1,7 @@
+package com.joje.dbee.common.utils;
+
+public class StringUtil {
+	public static boolean isBlank(String str) {
+		return str != null && str.length() > 0;
+	}
+}
