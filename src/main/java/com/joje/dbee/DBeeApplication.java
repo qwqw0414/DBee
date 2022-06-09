69e5b1af24aadb02a78cda2633e54750dae1f16a
qwqw0414 <xzdi0414@gmail.com>
2022-06-09 13:14:01 +0900

    의존성 추가

---
+++ src/main/java/com/joje/dbee/DBeeApplication.java
@@ -0,0 +1,13 @@
+package com.joje.dbee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DBeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DBeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joje/dbee/DBeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joje.dbee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DBeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
