12e52d54aea6db0bffafccae20077466541ad000
qwqw0414 <xzdi0414@gmail.com>
2022-06-09 13:47:50 +0900

    jasypt 추가

--- src/test/java/com/joje/dbee/DBeeApplicationTests.java
+++ src/test/java/com/joje/dbee/DBeeApplicationTests.java
@@ -1,5 +1,6 @@
 package com.joje.dbee;
 
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,4 +11,23 @@ class DBeeApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void jasypt() {
+		
+        String url = "jdbc:log4jdbc:mariadb://000.000.000.1:0000/dbee?characterEncoding=UTF-8&serverTimezone=UTC";
+        String username = "";
+        String password = "";
+
+        System.out.println(jasyptEncoding(url));
+        System.out.println(jasyptEncoding(username));
+        System.out.println(jasyptEncoding(password));
+    }
+	
+    public String jasyptEncoding(String value) {
+        String key = "joje_0414_95";
+        StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
+        pbeEnc.setAlgorithm("PBEWithMD5AndDES");
+        pbeEnc.setPassword(key);
+        return pbeEnc.encrypt(value);
+    }
 }
