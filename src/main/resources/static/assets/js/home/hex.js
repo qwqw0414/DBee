fe1ba5d85ad9cc635ab930f309d0d5661e0f3ae0
qwqw0414 <xzdi0414@gmail.com>
2022-06-13 15:05:34 +0900

    home 메뉴 디자인 완료

---
+++ src/main/resources/static/assets/js/home/hex.js
@@ -0,0 +1,20 @@
+function drawHex(oHexData) {
+    let sHtml = '';
+    if (oHexData && oHexData.length > 0) {
+        for (var i = 0; i < oHexData.length; i++) {
+            let sw = false;
+
+            let oHex = oHexData[i];
+            sHtml += '<li class="hex">';
+            sHtml += '<div class="hexIn">';
+            sHtml += '<a class="hexLink" href="' + (oHex.mapAddress ? oHex.mapAddress : '#') + '">';
+            // sHtml += '<img src="' + oHex.image + '" alt="" />';
+            sHtml += '<img src="/assets/img/home/default.jpg" alt="" />';
+            sHtml += '<p>' + (oHex.title ? oHex.title : '') + '</p>';
+            sHtml += '</a>';
+            sHtml += '</div>';
+            sHtml += '</li>';
+        }
+    }
+    return sHtml;
+}
